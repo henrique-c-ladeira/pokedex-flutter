301b6890f3b9cad5215d850eca34503caaf43ab9
Henrique <henrique.c.ladeira@gmail.com>
2022-02-14 21:18:22 -0300

    fix: entities

--- lib/entities/pokemon_details_entity.dart
+++ lib/entities/pokemon_details_entity.dart
@@ -1,9 +1,14 @@
+import 'package:myflutterapp/model/pokemon_details_model.dart';
+
 class PokemonDetailsEntity {
-  PokemonDetailsEntity({
-    required this.name,
-    required this.image,
-  });
+  PokemonDetailsEntity(
+      {required this.name,
+      required this.image,
+      required this.stats,
+      required this.type});
 
   final String name;
   final String image;
+  final List<Stats> stats;
+  final List<String> type;
 }
--- lib/entities/pokemon_entity.dart
+++ lib/entities/pokemon_entity.dart
@@ -1,6 +1,8 @@
 class PokemonEntity {
   final String name;
   final String image;
+  final String number;
 
-  PokemonEntity({required this.name, required this.image});
+  PokemonEntity(
+      {required this.name, required this.image, required this.number});
 }
