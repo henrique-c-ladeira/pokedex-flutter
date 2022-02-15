db2655a606a746b2e55ed758a4f367c8cbcfebc5
Henrique <henrique.c.ladeira@gmail.com>
2022-02-14 21:18:36 -0300

    feat: change limit pokemon list to 150

--- lib/repository/pokemon_list_repository.dart
+++ lib/repository/pokemon_list_repository.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:myflutterapp/constants/contants.dart';
 
 class PokemonListRepository {
-  final pokemonListUrl = '$baseUrl/pokemon/';
+  final pokemonListUrl = '$baseUrl/pokemon?limit=150';
 
   Future<List<dynamic>> getPokemonList() async {
     Response response = await Dio().get(pokemonListUrl);
